f7b8858109bf5b97a06c8adbd16d2ea9c888815c
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-05 15:30:03 +0100

    first commit

---
+++ pma.ps1
@@ -0,0 +1,10 @@
+﻿# Change this to the path of your phpMyAdmin installation
+$DBPath = "C:\Portable\phpMyAdmin-5.2.1-all-languages"
+$Port = "8080"
+$URL = "http://localhost:" + $Port
+$DBLink = "localhost:" + $Port
+
+Set-Location $DBPath
+Start-Process $URL
+php -S $DBLink
+
