8bf6f45e65a45b1d56039dfd9855b2470c51deea
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-05 16:08:35 +0100

    Add comments

--- pma.cmd
+++ pma.cmd
@@ -1,5 +1,8 @@
+@REM You can put this script in system environment variables path to lunch it with "pma" command 
 @echo off
+@REM Change this to the path of your phpMyAdmin installation
 set DBPath="C:\Portable\phpMyAdmin-5.2.1-all-languages"
+@REM Set the port you want (you can leave it like this)
 set Port="8080"
 set URL="http://localhost:%Port%"
 set DBLink="localhost:%Port%"
